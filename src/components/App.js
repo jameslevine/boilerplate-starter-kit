6b6a9eb7d370453cf4b2d2fe8069826471de1e3d
James Levine <jamesalexanderlevine@gmail.com>
2019-07-07 23:58:40 +0100

    added babel, jest, webpack, react router, redux to boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: black;
+  font-size: 2.5rem;
+  font-weight: 700;
+`;
+
+const App = () => <Title>Boilerplate Starter Kit</Title>;
+
+export default App;
